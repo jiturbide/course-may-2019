b0da0de84655a336306bab549d07476aa44b7307
jose luis iturbide <j_iturbide@hotmail.com>
2018-05-04 15:02:24 -0500

    Upload of corrections

--- jiturbide/20180427ejercicioFetch/index.js
+++ jiturbide/20180427ejercicioFetch/index.js
@@ -1,11 +1,11 @@
 (function() {
 
-    "use strict";
-	console.log("Inicio del script");
+  'use strict';
+	console.log('Inicio del script');
 
     const API_URL = 'datos.json';
 
-    const control = document.querySelector("#article0");
+    const control = document.querySelector('#article0');
 
      fetch('datos.json')
        .then(response => response.json())
@@ -13,24 +13,22 @@
        .catch(err => console.log('Fallo la peticion ' + err));
 
 	function printResponse(response) {
-		 	let theHtml = "";
-		 	let theRow = "";
+		 	let theHtml = '';
+		 	let theRow = '';
 
             theHtml = "<table style='width:100%' padd><tr> <th>Concepto</th> <th>Valor</th> </tr>";
 
             for(let key in response.data) {
                 //console.log(response.datos[key]['name']);
 
-                theRow = "<tr><td>" + response.data[key]['id'] + "</td><td>" + response.data[key]['name'] + "</td></tr>";
+                theRow = '<tr><td>' + response.data[key]['id'] + '</td><td>' + response.data[key]['name'] + '</td></tr>';
                 theHtml = theHtml + theRow;
             }
-            
-            theHtml = theHtml + "</table>";
+
+            theHtml = theHtml + '</table>';
 
 		 	control.innerHTML = theHtml;
 	}
 
 
 })();
-
-
