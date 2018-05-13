ef525000caf6217c58b6687078af92a1f6b9afef
jose luis iturbide <j_iturbide@hotmail.com>
2018-05-13 16:17:53 -0500

    JLIL Ajustes a sintaxis

--- jiturbide/20180427ejercicioFetch/index.js
+++ jiturbide/20180427ejercicioFetch/index.js
@@ -1,31 +1,30 @@
 (function() {
 
   'use strict';
-	console.log('Inicio del script');
 
-    const API_URL = 'datos.json';
+  const API_URL = 'datos.json';
 
-    const control = document.querySelector('#article0');
+  const control = document.querySelector('#article0');
 
-     fetch('datos.json')
-       .then(response => response.json())
-       .then(response => printResponse(response))
-       .catch(err => console.log('Fallo la peticion ' + err));
+   fetch('datos.json')
+     .then(response => response.json())
+     .then(jsondata => printResponse(jsondata))
+     .catch(err => console.log('Fallo la peticion ' + err));
 
-	function printResponse(response) {
+	function printResponse(jsondata) {
 		 	let theHtml = '';
 		 	let theRow = '';
 
-            theHtml = "<table style='width:100%' padd><tr> <th>Concepto</th> <th>Valor</th> </tr>";
+      theHtml = "<table style='width:100%' padd><tr> <th>Concepto</th> <th>Valor</th> </tr>";
 
-            for(let key in response.data) {
-                //console.log(response.datos[key]['name']);
+      for(let key in jsondata.data) {
+          //console.log(response.datos[key]['name']);
 
-                theRow = '<tr><td>' + response.data[key]['id'] + '</td><td>' + response.data[key]['name'] + '</td></tr>';
-                theHtml = theHtml + theRow;
-            }
+          theRow = '<tr><td>' + jsondata.data[key]['id'] + '</td><td>' + jsondata.data[key]['name'] + '</td></tr>';
+          theHtml = theHtml + theRow;
+      }
 
-            theHtml = theHtml + '</table>';
+      theHtml = theHtml + '</table>';
 
 		 	control.innerHTML = theHtml;
 	}
